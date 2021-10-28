e7df30292b4c4b369ae12baabbd745bfced73879
Luuk <luuk.volkering@gmail.com>
2021-10-28 17:21:44 +0200

    Made some changes to the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="nav" class="bg-fixed w-screen space-x-4 text-center text-gray-800 bg-blue-800 bg-opacity-80 text-xl p-2" style="background-image: ">
-    <router-link class="bg-yellow-400 p-1.5 rounded" to="/">Home</router-link>
-    <router-link class="bg-yellow-400 p-1.5 rounded" to="/projects">Projects</router-link>
-    <router-link class="bg-yellow-400 p-1.5 rounded" to="/cv">CV</router-link>
-    <router-link class="bg-yellow-400 p-1.5 rounded" to="/about">About</router-link>
+  <div id="app">
+    <div id="nav" class="bg-fixed w-screen space-x-4 text-center text-gray-800 bg-blue-500 bg-opacity-80 text-xl p-2 h-12">
+      <router-link class="bg-white p-1.5 rounded" to="/">Home</router-link>
+      <router-link class="bg-white p-1.5 rounded" to="/projects">Projects</router-link>
+      <router-link class="bg-white p-1.5 rounded" to="/cv">CV</router-link>
+      <router-link class="bg-white p-1.5 rounded" to="/about">About</router-link>
+    </div>
+    <div id="container" class="bg-grey-200">
+      <router-view/>
+    </div>
   </div>
-  <router-view/>
 </template>
