6663e61df4735faf1aaa895c977d36ecddf13655
Luuk <luuk.volkering@gmail.com>
2021-10-31 13:32:56 +0100

    Added AuthState function to protected pages

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,6 +1,37 @@
 <template>
     <div id="dashboard">
         <div v-if="authState !== 'signedin'">You're not allowed to see this page!</div>
-        <div v-if="authState === 'signedin' && user">Welcome to the dashboard!</div>
+        <div v-if="authState === 'signedin' && user">
+            Welcome to the dashboard!
+        </div>
     </div>
 </template>
+
+<script>
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+
+export default {
+  name: 'AuthStateApp',
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+        user: undefined,
+        authState: undefined,
+        unsubscribeAuth: undefined,
+        formFields: [
+            { type: "username" },
+            { type: "password" },
+            { type: "email" }
+        ],
+    }
+  },
+  beforeUnmount() {
+    this.unsubscribeAuth();
+  }
+}
+</script>
--- src/views/admin/Hours.vue
+++ src/views/admin/Hours.vue
@@ -6,3 +6,32 @@
         </div>
     </div>
 </template>
+
+<script>
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+
+export default {
+  name: 'AuthStateApp',
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+        user: undefined,
+        authState: undefined,
+        unsubscribeAuth: undefined,
+        formFields: [
+            { type: "username" },
+            { type: "password" },
+            { type: "email" }
+        ],
+    }
+  },
+  beforeUnmount() {
+    this.unsubscribeAuth();
+  }
+}
+</script>
