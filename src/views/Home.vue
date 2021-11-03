3432ad31835be284bcd33aee9d7b059a1a67b5ae
Luuk <luuk.volkering@gmail.com>
2021-11-03 21:58:56 +0100

    Continued playing around with css to come up with a design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="home-page" class="bg-gray-300">
+  <div id="home-page" class="w-full h-full bg-blue-300 text-gray-700 text-xl">
     <h1 class="text-center">Welcome to luuk180.dev!</h1>
+    <br />
     This is going to be my portfolio and I'm very busy building it right now!
   </div>
 </template>
