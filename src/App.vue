f13e6221376f6ff56330ea3d37b114a0340ba3c9
Luuk <luuk.volkering@gmail.com>
2021-11-04 14:23:31 +0100

    Made the colors look okay

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div v-if="authState !== 'signedin'">
-      <div id="nav" class="bg-fixed w-screen space-x-4 text-center text-gray-800 bg-blue-600 bg-opacity-80 text-2xl p-2 h-12">
+      <div id="nav" class="bg-fixed w-screen space-x-4 text-center text-gray-800 bg-blue-500 bg-opacity-80 text-2xl p-2 h-12">
         <router-link class="bg-white p-1.5 rounded" to="/">Home</router-link>
         <router-link class="bg-white p-1.5 rounded" to="/projects">Projects</router-link>
         <router-link class="bg-white p-1.5 rounded" to="/cv">CV</router-link>
@@ -16,7 +16,7 @@
         <button @click="handleSignOut" class="bg-white p-1.5 rounded w-6 right-4">Sign out</button>
       </div>
     </div>
-    <div id="container" class="bg-grey-200">
+    <div id="container" class="absolute inset-x-0 top-12 bottom-0 bg-blue-200">
       <router-view/>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="home-page" class="w-full h-full bg-blue-300 text-gray-700 text-xl">
+  <div id="home-page" class="text-gray-700 text-xl">
     <h1 class="text-center">Welcome to luuk180.dev!</h1>
     <br />
     This is going to be my portfolio and I'm very busy building it right now!
