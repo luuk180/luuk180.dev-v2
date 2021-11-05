131459e662233fc49d2c57438ed91d786afda9cf
Luuk <luuk.volkering@gmail.com>
2021-11-05 19:59:15 +0100

    Removed line break from home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="home-page" class="text-gray-700 text-xl">
     <h1 class="text-center">Welcome to luuk180.dev!</h1>
-    <br />
     This is going to be my portfolio and I'm very busy building it right now!
   </div>
 </template>
