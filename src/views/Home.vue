2860b47cf572944f5718334b5bbae20574f070c6
Luuk <luuk.volkering@gmail.com>
2022-01-18 18:21:34 +0100

    Simplified the navbar and started working on a homepage animation.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="home-page" class="text-gray-700 text-xl">
-    <h1 class="text-center">Welcome to luuk180.dev!</h1>
+    <h1 class="text-center">{{welcome_text}}</h1>
     This is going to be my portfolio and I'm very busy building it right now!
   </div>
 </template>
+
+<script>
+let actualtext = "Welcome to luuk180.dev!";
+let textfield = "";
+for(let i = 0; i > actualtext.length; i++) {
+  setTimeout(() => {textfield.appendChild(actualtext[i]);}, 1000);
+  welcome_text = textfield;
+}
+</script>
