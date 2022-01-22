4af0e003a291bb4a551dffd216955f16c1d1e892
Luuk <luuk.volkering@gmail.com>
2022-01-22 11:08:06 +0100

    Removed authenticated route

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { Auth } from 'aws-amplify'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import CV from '../views/CV.vue'
 import Projects from '../views/Projects.vue'
-import Login from '../views/Login.vue'
-
-function checkAuth(to, from) {
-  Auth.currentAuthenticatedUser({
-    bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
-  }).then(user => console.log(user))
-  .catch(err => {
-    console.log(err);
-    router.push(from).then()
-  })
-}
 
 const routes = [
   {
@@ -36,19 +24,6 @@ const routes = [
     path: '/projects',
     name: 'Projects',
     component: Projects
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: () => import('../views/admin/Dashboard.vue'),
-    beforeEnter: [checkAuth],
-    children: [
-    ]
   }
 ]
 
