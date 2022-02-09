6176dc63613127c2cba1005916a4fe4d497fffa3
Luuk <luuk.volkering@gmail.com>
2022-02-09 19:15:10 +0100

    The basis of the animated text is there I just need to perfect it.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,37 @@
 <template>
   <div id="home-page" class="text-gray-700 text-xl">
-    <h1 class="text-center">{{welcomeText}}</h1>
+    <h1 class="text-center">Welcome to my part of the internet!</h1>
     This is going to be my portfolio and I'm very busy building it right now!
   </div>
 </template>
 
+<style>
+h1 {
+    border-right: solid 3px rgba(0,255,0,.75);
+    white-space: nowrap;
+    overflow: hidden;
+    font-size: 28px;
+    color: rgba(255,255,255,.70);
+}
+
+h1 {
+    animation: animated-text 4s steps(29) 1s 1 normal both,
+                animated-cursor 600ms steps(29) infinite;
+}
+
+@keyframes animated-text{
+    from{width: 0;}
+    to {width: 472px;}
+}
+
+@keyframes animated-cursor{
+    from{border-right-color: rgba(0,255,0,.75);}
+    to{border-right-color: transparent;}
+}
+</style>
+
 <script>
     export default {
       name: "home",
-      props: {
-        welcomeText: "",
-      },
-      methods: {
-        animateWelcome() {
-          let finalText = "Welcome to luuk180.dev!";
-          for(let i = 0; i < finalText.length; i++) {
-            this.welcomeText.concat(finalText[i]);
-          }
-        }
-      },
-      created() {
-        this.animateWelcome();
-      }
     }
 </script>
