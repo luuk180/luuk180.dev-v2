5f1a545527b4bf60feefc24de59f2f806453d204
Luuk <luuk.volkering@gmail.com>
2022-02-14 11:18:20 +0100

    Added the animated text and made the page content look a little bit nicer.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,20 @@
 <template>
-  <div id="home-page" class="text-gray-700 text-xl">
-    <h1 class="text-center">Welcome to my part of the internet!</h1>
+  <div id="home-page" class="container mx-auto py-4 w-full text-gray-700 text-l">
+    <h1 id="welcome-text" class="w-full">Welcome to my part of the internet!</h1>
     This is going to be my portfolio and I'm very busy building it right now!
   </div>
 </template>
 
 <style>
-h1 {
-    border-right: solid 3px rgba(0,255,0,.75);
+#welcome-text {
+    border-right: solid 3px rgba(191, 219, 254, 1);
     white-space: nowrap;
     overflow: hidden;
     font-size: 28px;
-    color: rgba(255,255,255,.70);
+    color: rgba(55, 65, 81, 1);
 }
 
-h1 {
+#welcome-text {
     animation: animated-text 4s steps(29) 1s 1 normal both,
                 animated-cursor 600ms steps(29) infinite;
 }
@@ -25,7 +25,7 @@ h1 {
 }
 
 @keyframes animated-cursor{
-    from{border-right-color: rgba(0,255,0,.75);}
+    from{border-right-color: rgba(55,65,81,1);}
     to{border-right-color: transparent;}
 }
 </style>
