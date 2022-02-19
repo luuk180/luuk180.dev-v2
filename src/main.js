612fe1f67fa31f027afb47948de1c0090bd033ab
Luuk <luuk.volkering@gmail.com>
2022-02-19 13:07:00 +0100

    Added firebase hosting and functions

--- src/main.js
+++ src/main.js
@@ -4,6 +4,29 @@ import "tailwindcss/tailwind.css"
 import App from './App.vue';
 import './assets/tailwind.css'
 
-const app = createApp(App);
+//firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDyMdYiC8aH1onIWn4LhMM-ei-lI8XydL4",
+  authDomain: "luuk180-dev2.firebaseapp.com",
+  projectId: "luuk180-dev2",
+  storageBucket: "luuk180-dev2.appspot.com",
+  messagingSenderId: "1056784744633",
+  appId: "1:1056784744633:web:d40795f5c7bfaca7a29b68",
+  measurementId: "G-FYP5SFWER3"
+};
+
+// Initialize Firebase
+const fireApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(fireApp);
+
+const app = initializeApp(App);
 app.use(router)
 app.mount('#app');
