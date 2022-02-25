123d21de8a9a82589de8337cbe2f62cc6cfe4f31
Luuk <luuk.volkering@gmail.com>
2022-02-25 21:43:07 +0100

    Forgot to actually make it into the function that runs automatically

--- functions/index.js
+++ functions/index.js
@@ -6,47 +6,6 @@ admin.initializeApp();
 const db = admin.firestore();
 const GHcollection = db.collection("github");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest(async (request, response) => {
-  const responseTest = await fetch('https://api.github.com/graphql', {
-    method: 'POST',
-    headers: {
-      Authorization: `bearer ${functions.config().github.key}`
-    },
-    body: JSON.stringify({
-      query: `
-          {
-            user(login: "luuk180") {
-              repositories(orderBy: {field: PUSHED_AT, direction: ASC}, first: 100) {
-                edges {
-                  node {
-                    isPrivate
-                    name
-                    url
-                    homepageUrl
-                    description
-                    diskUsage
-                  }
-                }
-              }
-            }
-          }
-          `,
-    }),
-  });
-  const json = await responseTest.json();
-  const jsonObject = JSON.stringify(json.data.user.repositories.edges);
-  response.send(JSON.parse(jsonObject)[0].node);
-  for(var i = 0; i < JSON.parse(jsonObject).length; i++) {
-    const currObj = JSON.parse(jsonObject)[i];
-    if(!currObj.node.isPrivate) {
-      await GHcollection.doc(currObj.node.name).set(currObj.node);
-    }
-  }
-});
-
 // Fetch repository information from GitHub
 exports.GitHubToDB = functions.pubsub.schedule('0 * * * *')
   .timeZone('America/New_York').onRun(async () => {
@@ -76,8 +35,15 @@ exports.GitHubToDB = functions.pubsub.schedule('0 * * * *')
           `,
       }),
     });
-  const json = await response.json();
-  console.log(json.data.user.repositories.nodes);
+    const json = await response.json();
+    const jsonObject = JSON.stringify(json.data.user.repositories.edges);
+    response.send(JSON.parse(jsonObject)[0].node);
+    for(var i = 0; i < JSON.parse(jsonObject).length; i++) {
+      const currObj = JSON.parse(jsonObject)[i];
+      if(!currObj.node.isPrivate) {
+        await GHcollection.doc(currObj.node.name).set(currObj.node);
+      }
+    }
 
   return 0;
 });
