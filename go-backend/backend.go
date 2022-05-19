744d77ddca427de48946a5574aa369320578faac
Luuk <luuk.volkering@gmail.com>
2022-05-19 17:39:00 +0200

    Added a simple piece of text on the main route to the backend and made it, so you can only view the pdf file from the path

--- go-backend/backend.go
+++ go-backend/backend.go
@@ -8,10 +8,23 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the luuk180.dev backend!")
-	fs := http.FileServer(http.Dir("./files"))
+	//fs := http.FileServer(http.Dir("./files"))
 	http.HandleFunc("/query", handler)
-	http.Handle("/", fs)
+	//http.Handle("/file/", http.StripPrefix("/file", fs))
+	http.HandleFunc("/cv", handleCV)
+	http.HandleFunc("/", welcomeHandler)
 	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func handleCV(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./files/cv.pdf")
+}
+
+func welcomeHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "Welcome to the backend!")
+	if err != nil {
+		println(err)
+	}
 }
 
 func handler(w http.ResponseWriter, _ *http.Request) {
