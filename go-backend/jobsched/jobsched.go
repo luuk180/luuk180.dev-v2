ad0ca764d8c3968455521e186a0a2b32235d70e3
Luuk <luuk.volkering@gmail.com>
2022-06-01 15:53:16 +0200

    Connected to the database and wrote a script to update the table.

--- go-backend/jobsched/jobsched.go
+++ go-backend/jobsched/jobsched.go
@@ -6,10 +6,19 @@ import (
 	"runtime"
 )
 
+type ApiResult struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	URL         string `json:"url"`
+	HomepageURL string `json:"homepageUrl"`
+	Description string `json:"description"`
+	DiskUsage   int    `json:"diskUsage"`
+}
+
 func main() {
 	fmt.Println("Welcome to the luuk180.dev job scheduler!")
 
-	_, err := scheduler.Every(10).Seconds().Run(querySync)
+	_, err := scheduler.Every(10).Minutes().Run(querySync)
 	if err != nil {
 		return
 	}
