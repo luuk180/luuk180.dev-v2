e482e70c1e8dee944c774122c4b95ac7cb472197
Luuk <luuk.volkering@gmail.com>
2022-06-12 15:48:53 +0200

    Moved the back-end partially to the database and lambda but it's not automatically updating the database.

---
+++ go-backend/query/backend.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(event events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"},
+		Body:       getFromDb(),
+		StatusCode: http.StatusOK}
+	return &resp, nil
+}
