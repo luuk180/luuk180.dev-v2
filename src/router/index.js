2421e8a4704109126edca224d77d29423af1dde7
Luuk <luuk.volkering@gmail.com>
2022-11-01 15:33:57 +0100

    Added page for school

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import CV from '../views/CV.vue'
 import Projects from '../views/Projects.vue'
+import ShowPi from "@/views/ShowPi";
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: '/projects',
     name: 'Projects',
     component: Projects
+  },
+  {
+    path: '/pi',
+    name: "Pi List",
+    component: ShowPi
   }
 ]
 
---
+++ src/views/ShowPi.vue
@@ -0,0 +1,35 @@
+<template>
+ <div v-if="pis">
+     <ul class="list-group">
+         <div v-for="pi in pis" :key="pi.HostName">
+             <li class="list-group-item bg-secondary text-white">{{pi.Hostname}}: {{pi.LocalIp}}</li>
+         </div>
+     </ul>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "ShowPi",
+
+  data() {
+    return {
+      pis: [],
+    }
+  },
+
+  created() {
+    this.getPis();
+  },
+  methods: {
+    async getPis() {
+      const res = await fetch("https://api.luuk180.dev/getpis");
+      this.pis = await res.json();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
