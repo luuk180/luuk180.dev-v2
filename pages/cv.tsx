d349737969888c22efd08f96b64a9a1a7d80a179
Luuk <luuk.volkering@gmail.com>
2023-03-13 19:28:04 +0100

    Embedded cv pdf in webpage

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -4,9 +4,7 @@ export default function CV() {
     return <>
         <NavBar />
         <div className="container mx-auto py-4 w-full text-white text-l">
-            <p>
-                This will be my CV!
-            </p>
+            <iframe className="w-100 vh-100" src={"cv.pdf"} />
         </div>
     </>
 }
